eb51811600f52b765f309496fd80fe63ecfdf7f9
maysh <215359721@qq.com>
2020-07-27 14:34:17 +0800

    api

---
+++ src/example/apis/apis.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>apis</h1>
+    <div>{{message}}</div>
+    <button @click="updateBtn">更新</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'hello this is nextTick test'
+    }
+  },
+  methods: {
+    updateBtn: function () {
+      this.message = '更新了'
+    }
+  }
+}
+</script>
