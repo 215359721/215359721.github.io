94c91b4faf94abd008486d784b5b3c0624267d5a
215359721@qq.com <215359721@qq.com>
2021-11-25 13:26:07 +0800

    11-25-01

--- serverConfig.js
+++ serverConfig.js
@@ -1,6 +1,6 @@
 
 //当前部署版本号
-var base_version = '11_24_01'
+var base_version = '11_25_01'
 //流程地址
 var base_ip_flow = 'http://api.bjsasc.com/graph'
 //语义网地址
@@ -11,6 +11,10 @@ var grid_width_flow = 650
 var grid_height_flow = 180
 //泳道图网格-间距
 var grid_gap_flow = 5
+//移动偏移量-x
+var move_offset_x = 28
+//移动偏移量-y
+var move_offset_y = 1
 var config = {
   base_version,
   base_ip_flow,
@@ -18,5 +22,7 @@ var config = {
   grid_width_flow,
   grid_height_flow,
   grid_gap_flow,
+  move_offset_x,
+  move_offset_y,
 }
 window._SERVERCONF = config
