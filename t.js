b17c634763d807268ae8d96b02467007be562bfd
马寅生 <215359721@qq.com>
2024-03-18 12:32:08 +0800

    0318修改

--- t.js
+++ t.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  console.log('onloadOK')
+  console.log('onloadOK-0318 12:31')
   document.getElementById("openAppBtn").onclick = openApp;
   document.getElementById("callFunction").onclick = callFunc;
 
@@ -7,8 +7,16 @@ window.onload = function () {
     new QWebChannel(qt.webChannelTransport, function (channel) {
       window.bridge = channel.objects.bridge;
       // 启用按钮，确保此时 window.bridge 已经可用
-      document.getElementById("openAppBtn").disabled = false;
-      document.getElementById("callFunction").disabled = false;
+      // document.getElementById("openAppBtn").disabled = false;
+      // document.getElementById("callFunction").disabled = false;
+      if (window.bridge) {
+        window.bridge.dataChanged.connect(() => {
+          console.log('python成功调用了js方法')
+          alert('python成功调用了js方法')
+        })
+      } else {
+        console.log("bridge 对象未定义");
+      }
     });
   } else {
     console.log("QWebChannel 未定义，请检查 qwebchannel.js 是否已正确加载");
